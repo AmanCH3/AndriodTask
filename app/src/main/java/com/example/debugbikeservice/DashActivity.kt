269186392dbf8b50c1949641269cd52001a30617
AmanCH3 <armanachaudhary53@gmail.com>
2024-05-17 21:36:15 +0545

    feat : Dashboard activity and recycler view adapter completed

--- app/src/main/java/com/example/debugbikeservice/DashActivity.kt
+++ app/src/main/java/com/example/debugbikeservice/DashActivity.kt
@@ -5,16 +5,51 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.debugbikeservice.Fragment.HomeFragment
+import com.example.debugbikeservice.Fragment.ListFragment
+import com.example.debugbikeservice.Fragment.ProfileFragment
+import com.example.debugbikeservice.Fragment.SearchFragment
+import com.example.debugbikeservice.databinding.ActivityDashBinding
 
 class DashActivity : AppCompatActivity() {
+
+    lateinit var DashBinding: ActivityDashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        DashBinding = ActivityDashBinding.inflate(layoutInflater)
+        setContentView(DashBinding.root)
+
+        DashBinding.navMenu.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.search->replaceFragment(SearchFragment())
+                R.id.list -> replaceFragment(ListFragment())
+                R.id.Person->replaceFragment(ProfileFragment())
+                else->{}
+            }
+            true
         }
+
+
     }
+
+    private fun replaceFragment(fragment: Fragment ){
+        val fragmentManager: FragmentManager =supportFragmentManager
+        val fragmentTransaction: FragmentTransaction =fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+
+    }
+
 }
+
+
+
+
--- app/src/main/java/com/example/debugbikeservice/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/debugbikeservice/adapter/RecyclerAdapter.kt
@@ -1,5 +1,38 @@
 package com.example.debugbikeservice.adapter
 
-class RecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.debugbikeservice.R
+
+class RecyclerAdapter (var list : List<String> , var imgs : List<Int>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
+
+    class ViewHolder(var view : View) : RecyclerView.ViewHolder(view){
+
+        var title : TextView = view.findViewById(R.id.textName)
+        var imageView : ImageView = view.findViewById(R.id.imageView3)
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var  view = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent ,false)
+        return  ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text  = list[position]
+        holder.imageView.setImageResource(imgs[position])
+    }
+
+    override fun getItemCount(): Int {
+        return  list.size
+    }
+
+
 
 }
